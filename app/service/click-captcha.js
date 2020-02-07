56cf6da026b79f2c81434fdba91defd03cc775c2
root <root@localhost.localdomain>
2020-02-07 06:33:37 -0500

    test rotate text

--- app/service/click-captcha.js
+++ app/service/click-captcha.js
@@ -27,23 +27,40 @@ class ClickCaptchaService extends Service {
     Canvas.registerFont(fontFile('PfennigItalic.ttf'), { family: 'pfennigFont', style: 'italic' });
     Canvas.registerFont(fontFile('PfennigBoldItalic.ttf'), { family: 'pfennigFont', weight: 'bold', style: 'italic' });
 
-    const canvas = Canvas.createCanvas(320, 320);
+    const canvas = Canvas.createCanvas(360, 400);
     const context = canvas.getContext('2d');
     const image = await Canvas.loadImage(imageFile('lime-cat.jpg'));
 
+    context.save();
     context.drawImage(image, 0, 0);
-
     context.font = 'normal normal 50px Helvetica';
-    context.fillText('中', 0, 70);
+    context.rotate(Math.PI / 6);
+    context.fillText('中', 70, 70);
+    const t1 = context.measureText('中');
+    context.restore();
+    console.log(t1);
 
+
+    context.save();
     context.font = 'bold 50px pfennigFont';
-    context.fillText('国', 70, 140, 100);
+    context.rotate(270);
+    context.fillText('国', 140, 140, 100);
+    const t2 = context.measureText('国');
+    console.log(t2);
+    context.restore();
 
+    context.save();
     context.font = 'italic 50px pfennigFont';
-    context.fillText('加', 140, 210);
+    context.fillText('加', 210, 210);
+    const t3 = context.measureText('加');
+    console.log(t3);
+    context.restore();
 
     context.font = 'bold italic 50px pfennigFont';
-    context.fillText('油', 210, 280);
+    context.fillText('油', 280, 280);
+    const t4 = context.measureText('油');
+    console.log(t4);
+    context.save();
 
     return canvas.toBuffer('image/jpeg', { quality: 0.5 });
   }
